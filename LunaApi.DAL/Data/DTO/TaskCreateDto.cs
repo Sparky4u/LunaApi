76bacdd328e3cc86dead261b4f8c5f62b07b70e0
Вадим Корнелюк <V.Kornelyuk_FIT_13_21_B_d@knute.edu.ua>
2024-09-17 19:30:17 +0300

    deleted useless usings

--- LunaApi.DAL/Data/DTO/TaskCreateDto.cs
+++ LunaApi.DAL/Data/DTO/TaskCreateDto.cs
@@ -1,9 +1,4 @@
 ﻿using LunaApi.Common.Enums;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace LunaApi.DAL.Data.DTO
 {
--- LunaApi.DAL/Data/Interfaces/IAuthService.cs
+++ LunaApi.DAL/Data/Interfaces/IAuthService.cs
@@ -1,9 +1,4 @@
 ﻿using LunaApi.Common.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace LunaApi.DAL.Data.Interfaces
 {
